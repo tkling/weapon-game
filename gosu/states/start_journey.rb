8a3a649ea73f3281b88f69e0a3b871e5ff028911
Tyler Kling <tyling@gmail.com>
2016-12-10 19:40:30 -0500

    draw dungeon info

--- gosu/states/start_journey.rb
+++ gosu/states/start_journey.rb
@@ -14,7 +14,7 @@ class StartJourney < GameState
 
   def draw
     # draw party info
-    @x_padding ||= 60
+    @x_padding ||= 40
     @x_starts ||= [0, @window.width/3, @window.width * 0.66667].map { |i| i + @x_padding }
     @x_starts.each_with_index do |x, idx|
       partymember = @window.globals.party[idx]
@@ -24,15 +24,20 @@ class StartJourney < GameState
       max_hp = "Max HP: #{ partymember.max_hp }"
       curr_hp = "Current HP: #{ partymember.current_hp }"
 
-      @window.large_font_draw(x, 15, 0, Color::YELLOW, partymember.name)
-      @window.small_font_draw(x, 60, 30, Color::YELLOW, weapon_name, armor_name, total_atk, max_hp, curr_hp)
+      @window.large_font_draw(x, 25, 0, Color::YELLOW, partymember.name)
+      @window.small_font_draw(x, 65, 30, Color::YELLOW, weapon_name, armor_name, total_atk, max_hp, curr_hp)
     end
 
-    # show current dungeon
-    # need to add dungeon stuff, lol!
+    # show current map + dungeon
+    @map_name ||= "Map: #{ map.name }"
+    @current_dungeon ||= "Current dungeon: #{ dungeon.name }"
+    @encounters_completed ||= "Encounters completed dungeon: #{ dungeon.encounter_index }/#{ dungeon.encounter_count}"
+
+    @window.large_font_draw(@x_padding, @window.height - 300, 0, Color::YELLOW, @map_name)
+    @window.normal_font_draw(@x_padding, @window.height - 250, 20, Color::YELLOW, @current_dungeon, @encounters_completed)
 
     # show confirmation
     @continue_msg ||= 'press q to continue with this party'
-    @window.large_font_draw(170, @window.height - 100, 0, Color::YELLOW, @continue_msg)
+    @window.large_font_draw(170, @window.height - 130, 0, Color::YELLOW, @continue_msg)
   end
 end
