64371d445fb545d5615c3793978ff874e36624cb
tkling <tyling@gmail.com>
2020-07-23 08:21:07 -0400

    move save logic into SaveMethods mixin

---
+++ gosu/lib/save_methods.rb
@@ -0,0 +1,32 @@
+module SaveMethods
+  def save(save_name)
+    Dir.mkdir save_dir unless File.directory? save_dir
+    save_path = File.join(save_dir, save_name)
+
+    File.write(save_path, savefile_json)
+    window.globals.save_data.updated_on = Time.now
+    window.globals.save_data.filename = save_name
+
+    @file_saved = true
+  end
+
+  def file_saved?
+    !!@file_saved
+  end
+
+  def save_dir
+    @save_dir ||= File.join(window.project_root, 'saves')
+  end
+
+  def savefile_json
+    JSON.pretty_generate({
+      players: window.globals.party.map(&:to_h),
+      map: window.globals.map.to_h,
+      time_played: 10,
+   })
+  end
+
+  def savefile_paths
+    Dir[File.join(save_dir, '*.save')]
+  end
+end
