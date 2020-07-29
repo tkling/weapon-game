0e16e366ee9001a9de33930a352bd32c71246ade
tkling <tyling@gmail.com>
2020-07-29 15:46:23 -0400

    flesh out inventory with names + counts

--- gosu/states/inventory.rb
+++ gosu/states/inventory.rb
@@ -4,10 +4,21 @@ class Inventory < GameState
   end
 
   def draw
-    message = 'Come back later~'
-    window.huge_font_draw(window.width/3, window.height/3, 0, Color::YELLOW, message)
+    window.huge_font_draw(10, 10, 0, Color::YELLOW, 'I N V E N T O R Y')
+    window.large_font_draw(50, 100, 0, Color::YELLOW, 'item name : count')
+
+    x_item, x_count = 50, 500
+    y_both = 150
+
+    inventory.tally.each do |item, count|
+      count = count < 10 ? ":0#{count}" : ":#{count}"
+
+      window.normal_font_draw(x_item,  y_both, 0, Color::YELLOW, item)
+      window.normal_font_draw(x_count, y_both, 0, Color::YELLOW, count)
+      y_both += 30
+    end
 
     press_space = 'Press [space] to return to caravan menu.'
-    window.large_font_draw(window.width/3-200, window.height/3+100, 0, Color::YELLOW, press_space)
+    window.large_font_draw(window.width/2-250, window.height-100, 0, Color::YELLOW, press_space)
   end
 end
