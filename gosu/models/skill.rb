1d6e1419781bf6b1b1cc587e0b3f3ac0c56ddcca
tkling <tyling@gmail.com>
2020-08-09 13:13:54 -0400

    bug: forgot to default xp for newgame skills

--- gosu/models/skill.rb
+++ gosu/models/skill.rb
@@ -15,7 +15,7 @@ class Skill < CastleModel
   end
 
   protected
-  def initialize(id:, name:, type:, element:, str:, max_level:, xp:, xp_thresholds:, level_modifier:)
+  def initialize(id:, name:, type:, element:, str:, max_level:, xp: 0, xp_thresholds:, level_modifier:)
     @id, @name, @type, @xp          = id, name, type, xp
     @element, @str, @max_level      = element, str, max_level
     @xp_thresholds, @level_modifier = xp_thresholds, level_modifier
