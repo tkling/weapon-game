c4b89f8f27fa5d903be2f4f02effb308a1a6a80f
tkling <tyling@gmail.com>
2020-08-09 13:09:45 -0400

    bug: forgot to save item hashes instead of inventory array after castle conversion

--- gosu/lib/save_methods.rb
+++ gosu/lib/save_methods.rb
@@ -22,7 +22,7 @@ module SaveMethods
     JSON.pretty_generate({
       players: window.globals.party.map(&:to_h),
       map: window.globals.map.to_h,
-      inventory: window.globals.inventory,
+      inventory: window.globals.inventory.map(&:to_h),
       time_played: Time.now - window.globals.session_begin_time + window.globals.save_data.time_played,
    })
   end
