cfb4d03e32b2f9b24d40a1778dda5a5e82717541
tkling <tyling@gmail.com>
2020-09-07 18:38:03 -0400

    use SelectableChoiceList instead of numbered keys

--- gosu/states/continue.rb
+++ gosu/states/continue.rb
@@ -3,37 +3,35 @@ class Continue < GameState
 
   def initialize(window)
     super window
-    @save_map = save_map
     @draw_time = Time.now
+    @savefile_count = savefile_paths.size
+    @choice_list = SelectableChoiceList.new(
+      parent_screen: self,
+      choice_mappings: savefile_paths.map.with_index do |filename, idx|
+        { text: savefile_display_string(filename, idx+1), action: ->{ load_and_continue(filename) } }
+      end
+    )
+  end
+
+  def savefile_display_string(filename, slot_number)
+    playtime = formatted_time_played JSON.parse(File.read(filename))['time_played']
+    "#{slot_number} - #{filename.split('/').last} - playtime: #{playtime}"
   end
 
   def draw
     window.huge_font_draw(25, 10, 0, Color::YELLOW, 'CHOOSE A SAVE')
 
-    if savefile_paths.size > 0
-      starting_key = 0
-      subbed = savefile_paths[0..8].map do |filename|
-        playtime_str = formatted_time_played JSON.parse(File.read(filename))['time_played']
-        "#{starting_key += 1} - #{filename.split('/').last} - playtime: #{playtime_str}"
-      end
-      window.normal_font_draw(15, 100, 40, Color::YELLOW, *subbed)
+    if @savefile_count.positive?
+      @choice_list.draw(x: 50, y_start: 100, y_spacing: 40, draw_method: :normal_font_draw)
     else
       window.large_font_draw(15, 100, 0, Color::YELLOW, 'NO SAVES YET')
       window.normal_font_draw(15, 140, 0, Color::YELLOW, 'Continuing to new game now...')
-      @no_saves = true
     end
   end
 
   def update
-    if @no_saves && Time.now - @draw_time > 1
+    if @savefile_count.zero? && Time.now - @draw_time > 1
       proceed_to NewGame
-    end
-  end
-
-  def bind_keys
-    (1..9).each do |i|
-      key = Module.const_get("Keys::Row#{i}")
-      bind key, ->{ load_and_continue(@save_map[key]) }
     end
   end
 
@@ -54,11 +52,8 @@ class Continue < GameState
       Character.new **character_info
     end
 
-    if players.size > 0
-      window.globals.party = players
-    else
-      raise 'something failed while reading, size 0'
-    end
+    raise 'something failed while reading, party size is 0' if players.size.zero?
+    window.globals.party = players
   end
 
   def set_map_from_hash(hash)
@@ -70,12 +65,5 @@ class Continue < GameState
 
   def set_inventory_from_hash(hash)
     window.globals.inventory = hash[:inventory]&.map { |item_hash| Item.from_castle_id(item_hash[:id]) } || []
-  end
-
-  def save_map
-    savefile_paths.each_with_index.with_object(Hash.new) do |(filename, idx), map|
-      break if idx == 9
-      map[Module.const_get("Keys::Row#{idx + 1}")] = filename
-    end
   end
 end
