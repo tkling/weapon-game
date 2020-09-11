114dca5dbe9304ebb63d8851d7427fbfe6d9cab3
tkling <tyling@gmail.com>
2020-09-11 00:05:46 -0400

    placeholder code for attack misses

--- gosu/models/damage.rb
+++ gosu/models/damage.rb
@@ -17,12 +17,18 @@ class Damage
     # at least they don't make it known/obvious if enemies have similar equipables to what are available to player
     # characters. It could potentially simplify things to have element/resist/damage directly on enemies rather than
     # coming from their equipables. Then again there's a nice simplicity to players and enemies sharing the same code.
+    return 0 if should_miss?
     damage = from.type == 'partymember' ? -10 : (-5..-1).to_a.sample
     damage = damage * Elements::AffinityMap[source.element][to.armor.element]
     damage = damage * source.level_damage_multiplier
     damage = damage * (100 - to.armor.damage_resist) / 100
     damage = damage.round(half: :up).to_i
     damage == 0 ? 1 : damage
+  end
+
+  def should_miss?
+    # placeholder for now, later base this on comparing from vs to dex, maybe also status effects
+    false
   end
 
   def message
